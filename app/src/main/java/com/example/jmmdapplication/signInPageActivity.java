81c40fdb0f20ec06d31e25b3c3ca4ce1accb13a6
JerrickWallace <jerrick005@yahoo.com>
2024-07-30 15:23:14 -0700

    Implemented starter code for sign in page and button.

--- app/src/main/java/com/example/jmmdapplication/signInPageActivity.java
+++ app/src/main/java/com/example/jmmdapplication/signInPageActivity.java
@@ -3,6 +3,8 @@ package com.example.jmmdapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,16 +15,47 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.jmmdapplication.databinding.ActivitySignInPageBinding;
 
 public class signInPageActivity extends AppCompatActivity {
+
     private ActivitySignInPageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in_page);
         binding=ActivitySignInPageBinding.inflate(getLayoutInflater());
-
         setContentView(binding.getRoot());
 
+        binding.signInButtonSignInPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleSignIn();
+            }
+        });
+
+        binding.signUpButtonSignInPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleSignUp();
+            }
+        });
+
     }
+
+    private void handleSignIn() {
+        String username = binding.userNameSignInScreenEditText.getText().toString();
+        String password = binding.PasswordSignInEditText.getText().toString();
+
+        if(validateCredentials(username,password)) {
+
+            Intent intent = MainUserInterface.MainUserInterfaceIntentFactory(this);
+            startActivity(intent);
+        }else {
+            Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean validateCredentials(String username, String password) {
+        return "user".equals(username) && "password".equals(password);
+    }
+
     static Intent intentFactory(Context context){
         return new Intent(context, signInPageActivity.class);
     }
