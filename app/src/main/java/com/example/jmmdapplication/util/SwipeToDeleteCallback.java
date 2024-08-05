101bedb4cbb381676ba0193be106dda9f5a99f2d
JerrickWallace <jerrick005@yahoo.com>
2024-08-05 14:35:40 -0700

    Created a ItemTouchHelper callback.

---
+++ app/src/main/java/com/example/jmmdapplication/util/SwipeToDeleteCallback.java
@@ -0,0 +1,38 @@
+package com.example.jmmdapplication.util;
+
+import android.graphics.Canvas;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.jmmdapplication.UserAdapter;
+
+public class SwipeToDeleteCallback extends ItemTouchHelper.SimpleCallback {
+    private final UserAdapter mAdapter;
+
+
+    public SwipeToDeleteCallback(UserAdapter adapter) {
+        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        mAdapter = adapter;
+
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        int position = viewHolder.getAdapterPosition();
+        mAdapter.deleteItem(position);
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder,
+                            float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
+}
+
