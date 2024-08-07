5bc3bcbe0c94f7c530c514434b40f0486b64be4e
matthewross <mattross@csumb.edu>
2024-08-06 19:14:46 -0700

    updated add challenges screen to display proper buttons

--- app/src/main/java/com/example/jmmdapplication/AddNewChallenge.java
+++ app/src/main/java/com/example/jmmdapplication/AddNewChallenge.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.jmmdapplication.Database.entities.Challenge;
+import com.example.jmmdapplication.Database.repository.DatabaseRepository;
 import com.example.jmmdapplication.databinding.ActivityAddnewChallengeBinding;
+
+import java.util.ArrayList;
+
 /**
  * Activity that allows users to add a new challenge.
  * <p>
@@ -17,7 +23,7 @@ import com.example.jmmdapplication.databinding.ActivityAddnewChallengeBinding;
  */
 public class AddNewChallenge extends AppCompatActivity {
     private ActivityAddnewChallengeBinding binding;
-
+    private DatabaseRepository repository=DatabaseRepository.getRepository(this.getApplication());
     /**
      * Called when the activity is first created.
      * <p>
@@ -39,8 +45,31 @@ public class AddNewChallenge extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+        binding.SearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.enterLanguageBar.getText();
+            }
+        });
+        binding.button1.setOnClickListener();
 
+
+    }
+    private void displayChallenge(String language){
+        int check=0;
+
+        for(Challenge challenge:repository.getAllChallenges()){
+            if(challenge.getCategory().equalsIgnoreCase(language)){
+                String temp= challenge.getCategory()+"\n"+challenge.getDescription();
+                binding.button1.setText(temp);
+                binding.button1.setVisibility(View.VISIBLE);
+                check =1;
+            }
+        }
+        if (check==0){
+            Toast.makeText(this, "Language not Found", Toast.LENGTH_SHORT).show();
+        }
+    }
     /**
      * Factory method to create an {@link Intent} for starting {@link AddNewChallenge}.
      *
