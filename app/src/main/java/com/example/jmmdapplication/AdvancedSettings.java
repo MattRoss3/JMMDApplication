a9240ff6fdff6a9e15c4a7e56915e99a42264d15
matthewross <mattross@csumb.edu>
2024-08-07 23:53:23 -0700

    started editing advanced settings

--- app/src/main/java/com/example/jmmdapplication/AdvancedSettings.java
+++ app/src/main/java/com/example/jmmdapplication/AdvancedSettings.java
@@ -8,17 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AdvancedSettings extends AppCompatActivity {
+import com.example.jmmdapplication.databinding.ActivityAdvancedSettingsBinding;
 
+public class AdvancedSettings extends AppCompatActivity {
+    ActivityAdvancedSettingsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_advanced_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding=
+
         });
     }
 }
